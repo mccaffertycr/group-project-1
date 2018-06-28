53d34f0bd6f67fd538e89accd6905b82dce97b1c
Megan Anthony <megananthony@Megans-MacBook-Air.local>
2018-06-27 20:54:21 -0400

    spofity js, tokens only

--- javascript.js
+++ javascript.js
@@ -1,25 +1,23 @@
 $(document).ready(function () {
     console.log("ready!");
 
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/api/token" 
+    var clientId = "1734fafdd46a492594d75bd172d27d33"
+    var secret = "76fc2f6fd0454a5ab0ec014a3a42fa19"
+    var bas64 = btoa(  clientId +  ":" + secret )
 
-    //GET https://www.googleapis.com/youtube/v3/search?part=snippet&topicId=/m/05z1_&type=video&key=AIzaSyCfZI_vpTv4-aT3219PMkDee1Z_iaocTkQ
-
-    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&topicId=/m/04rlf" + "&type=video&key=AIzaSyCfZI_vpTv4-aT3219PMkDee1Z_iaocTkQ";
+    //console.log(btoa(  clientId +  ":" + secret ))
     $.ajax({
+        beforeSend: function(request) {
+            request.setRequestHeader("Authorization", "Basic " + bas64)
+        },
         url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response.items);
-
-        for (var i = 0; i < response.items.length; i++) {
-            $("#results").append("<iframe id='player' type='text/html' width='640' height='390'"+
-            "src='http://www.youtube.com/embed/"+ response.items[i].id.videoId + 
-             "?enablejsapi=1&origin=http://example.com'"+
-            "frameborder='0'></iframe>'")
-        }
-
-    })
-
-
-
+        method: "POST",
+        data: {grant_type: "client_credentials"}
+        // xhrFields: { withCredentials: true}
+    }).done(function (response) {
+        console.log(response);
+    }).fail(function() {
+       console.log('something failed'); 
+    });
 });
