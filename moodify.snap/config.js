1faa85025b5d2670235196af9e1a7113835a3eee
mccaffertycr <mccaffertycr@gmail.com>
2018-06-30 16:54:21 -0400

    moved button listener out of config

--- moodify.snap/config.js
+++ moodify.snap/config.js
@@ -11,27 +11,4 @@ $('#save-keys').on('click', function() {
   $('#api-keys').empty();
 })
 
-// hide main container when page loads
-$('#main').hide();
 
-$('#get-started').on('click', function() {
-
-  // hide the canvas
-  $('#canvas').hide();
-  // remove current mood 
-  $('#mood-label').remove();
-  // empty any existing playlists
-  $('#playlist-wrapper').empty();
-  // display the main container and main-wrapper 
-  $('#main').show();
-  $('#main-wrapper').show();
-  // adjust text on header button
-  $('#get-started').text('get started'); 
-
-  // start webcam
-  navigator.mediaDevices.getUserMedia(constraints)
-        .then((stream) => {
-            // Attach the video stream to the video element and autoplay.
-            player.srcObject = stream;
-        });
-})
--- moodify.snap/getmood.js
+++ moodify.snap/getmood.js
@@ -112,7 +112,35 @@
         return new Blob([uInt8Array], { type: contentType });
     }
 
+    // hide main container when page loads
+    $('#main').hide();
+    
+    // 
+    // 
     // event listeners
+    // 
+    // 
+
+    $('#get-started').on('click', function() {
+    // hide the canvas
+    $('#canvas').hide();
+    // remove current mood 
+    $('#mood-label').remove();
+    // empty any existing playlists
+    $('#playlist-wrapper').empty();
+    // display the main container and main-wrapper 
+    $('#main').show();
+    $('#main-wrapper').show();
+    // adjust text on header button
+    $('#get-started').text('get started'); 
+
+    // start webcam
+    navigator.mediaDevices.getUserMedia(constraints)
+            .then((stream) => {
+                // Attach the video stream to the video element and autoplay.
+                player.srcObject = stream;
+            });
+    })
 
     $(document).on('click', '#capture', () => {
         $('#canvas').show();
