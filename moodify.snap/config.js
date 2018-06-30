eb8b06299e87d64ae846b980362912635169d2b4
mccaffertycr <mccaffertycr@gmail.com>
2018-06-30 16:12:00 -0400

    working on a start button

--- moodify.snap/config.js
+++ moodify.snap/config.js
@@ -1,24 +1,19 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDv49wCu5KM8OdclT6-gZtfvZzM98ahMas",
-    authDomain: "moodify-76b22.firebaseapp.com",
-    databaseURL: "https://moodify-76b22.firebaseio.com",
-    projectId: "moodify-76b22",
-    storageBucket: "moodify-76b22.appspot.com",
-    messagingSenderId: "310383968549"
-  };
-  firebase.initializeApp(config);
-
 var subscriptionKey = '';
 var clientId = '';
 var secret = '';
 
-$('#save-keys').on('click', function() {
+// event listener that collects api keys
 
+$('#save-keys').on('click', function() {
   subscriptionKey = $('#emotion-api-key-input').val().trim();
   clientId = $('#spotify-id-input').val().trim();
   secret = $('#spotify-secret-input').val().trim();
   $('#api-keys').empty();
-
 })
 
+// hide main container when page loads
+// $('#main').hide();
+
+$('#get-started').on('click', function() {
+
+})
